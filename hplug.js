9cba82e2eed09c7bfae98f67c1d25397960ed98e
SirMeerkat <paulhuynh94@gmail.com>
2016-06-15 14:28:15 -0700

    Add files via upload

---
+++ hplug.js
@@ -0,0 +1,9 @@
+﻿//Copyright Notice
+//Copyright (c) 2016 Paul Huynh
+
+function createNewTab() {
+    chrome.tabs.create({url : 'http://google.com'});
+    chrome.tabs.getCurrent(function(tab){
+            chrome.tabs.remove(tab.id);
+    })
+}   
