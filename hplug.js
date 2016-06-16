c6a79c9fa87e56e3d915146e1919ad1f50814600
Jwong <jimmyw487@gmail.com>
2016-06-15 18:08:27 -0700

    Added background, added history remove, updated manifest

--- hplug.js
+++ hplug.js
@@ -2,12 +2,25 @@
 //Copyright (c) 2016 Paul Huynh
 
 function createNewTab() {
-    chrome.tabs.create({url : 'http://google.com'});
-    chrome.tabs.getCurrent(function(tab){
-            chrome.tabs.remove(tab.id);
-    })
+    chrome.tabs.create({ url: 'http://google.com' });
+    removeCurrentTab();
+};
+
+function removeCurrentTab() {
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+        chrome.tabs.remove(tabs[0].id, function () { });
+    });
+};
+
+function removeLastHour() {
+    var end = Date.now();
+    var start = end - 3600000;
+    chrome.history.deleteRange({ startTime: start, endTime: end }
+        , function () { });
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-  createNewTab();
+    createNewTab();
+    removeCurrentTab();
+    removeLastHour();
 });
