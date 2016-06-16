0066853156d028068431db4ed1e18b9ec5a47d85
Aitorz <Aitorz@users.noreply.github.com>
2016-06-15 18:11:53 -0700

    remove tab in js updated, manifest updated

--- hplug.js
+++ hplug.js
@@ -3,11 +3,20 @@
 
 function createNewTab() {
     chrome.tabs.create({ url: 'http://google.com' });
+<<<<<<< HEAD
+    removeCurrentTab();
+};
+
+function removeCurrentTab() {
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+        chrome.tabs.remove(tabs[0].id, function () { });
+=======
 };
 
 function removeCurrentTab() {
     chrome.tabs.getCurrent(function (tab) {
         chrome.tabs.remove(tab.id, function () { });
+>>>>>>> refs/remotes/origin/master
     });
 };
 
@@ -16,12 +25,19 @@ function removeLastHour() {
     var start = end - 3600000;
     chrome.history.deleteRange({ startTime: start, endTime: end }
         , function () { });
+<<<<<<< HEAD
+};
+=======
 }
+>>>>>>> refs/remotes/origin/master
 
 document.addEventListener('DOMContentLoaded', function () {
     createNewTab();
     removeCurrentTab();
     removeLastHour();
+<<<<<<< HEAD
+=======
 
 
+>>>>>>> refs/remotes/origin/master
 });
